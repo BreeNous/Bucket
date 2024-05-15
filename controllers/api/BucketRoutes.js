717ba32663a1537dabf290643ddc71dfb7836b11
sjastrow <skjastrow@gmail.com>
2024-05-15 12:57:09 -0400

    created seeds.js. created db.

--- controllers/api/BucketRoutes.js
+++ controllers/api/BucketRoutes.js
@@ -4,4 +4,41 @@
 
 const router = require('express').Router();
 const { Project } = require('../../models');
-const withAuth = require('../../utils/auth');
+const withAuth = require('../../utils/auth');;
+
+// If a POST request is made to /api/projects, a new project is created. If there is an error, the function returns with a 400 error. 
+router.post('/', async (req, res) => {
+  try {
+    const newProject = await Project.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newProject);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// If a DELETE request is made to /api/projects/:id, that project is deleted. 
+router.delete('/:id', async (req, res) => {
+  try {
+    const projectData = await Project.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!projectData) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
+    }
+
+    res.status(200).json(projectData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ seeds/seed.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { User, BucketListItem } = require('../models');
+
+// const userData = require('./userData.json');
+const BucketListItems = require('./BucketListItem.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+  for (const bucketListItem of BucketListItems) {
+    await BucketListItem.create({
+      ...bucketListItem,
+    //   user_id: users[Math.floor(Math.random() * users.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
