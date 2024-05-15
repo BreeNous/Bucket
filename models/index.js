83cad9ac56654845544e5b57be7bccf4d25fa2f9
sjastrow <skjastrow@gmail.com>
2024-05-15 11:00:33 -0400

    refactored project.js to bucketlistitem.

--- models/index.js
+++ models/index.js
@@ -1,15 +1,15 @@
 const User = require('./User');
-const Project = require('./Project');
+const BucketListItem = require('./BucketListItem');
 
 // Creates a relationship between User and Project model, with the User having a "has many" relationship with Project model.
-User.hasMany(Project, {
+User.hasMany(BucketListItem, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
 // Creates a relationship between User and Project model, with a "belongs to" relationship of the Project to the User.
-Project.belongsTo(User, {
+BucketListItem.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Project };
+module.exports = { User, BucketListItem };
