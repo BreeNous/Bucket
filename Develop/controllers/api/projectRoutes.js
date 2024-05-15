a240d81687fae4a7a8e8d43880b54506d3687c5a
sjastrow <skjastrow@gmail.com>
2024-05-14 20:20:42 -0400

    added public folder, with css and js folders inside of it. completed profile.js, login.js, and logout.js.

--- Develop/controllers/api/projectRoutes.js
+++ Develop/controllers/api/projectRoutes.js
@@ -1,3 +1,7 @@
 //favorited bl items. call to find in bucket lsit where id = to get favorites
 
 //users bucket list. call based on user id
+
+const router = require('express').Router();
+const { Project } = require('../../models');
+const withAuth = require('../../utils/auth');
