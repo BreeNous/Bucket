b328bdde587c88695e5c7997016daa3ed7db567e
Dorphia <alex.rybak333@gmail.com>
2024-05-15 11:14:57 -0400

    Refactored the routes and updated BucketRoutes.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./BucketRoutes');
+const BucketRoutes = require('./BucketRoutes');
 
 router.use('/users', userRoutes);
-router.use('/projects', projectRoutes);
+router.use('/bucket', BucketRoutes);
 
 module.exports = router;
