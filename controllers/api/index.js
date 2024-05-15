1bfec7c4407346934d95430785c5dcd0dcde20bf
sjastrow <skjastrow@gmail.com>
2024-05-15 11:00:33 -0400

    refactored project.js to bucketlistitem.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const projectRoutes = require('./BucketRoutes');
 
 router.use('/users', userRoutes);
 router.use('/projects', projectRoutes);
