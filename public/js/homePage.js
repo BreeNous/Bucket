983def85ad4c096fbc6a922cc01627556866da40
BreeNous <breeanna.nouskhajian@gmail.com>
2025-02-24 15:41:19 -0600

    restyled and restructured homePage, and all its redirect properties

---
+++ public/js/homePage.js
@@ -0,0 +1,5 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modals = document.querySelectorAll(".modal");
+    M.Modal.init(modals); // Initialize Materialize modals
+});
---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+// sign up form handler to get user data.
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    // if requirements met, create user
+    if (name && email && password) {
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
